df740eee46944a131d60279abc5bf9a8582e3079
David Holdeman <dave@holdemanenterprises.com>
2023-09-08 21:51:01 -0500

    Allow on linked bodies in Assembly4 too

--- StraightCut.py
+++ StraightCut.py
@@ -7,19 +7,105 @@ class StraightCut():
         self.Type = "straightcut"
         obj.addProperty("App::PropertyLink", "Part", "StraightCut", "Object to be cut")
         obj.addProperty("App::PropertyLink", "Tool", "StraightCut", "Object which which the part intersects")
+        # Very hacky storing as strings to avoid loops.
+        obj.addProperty("App::PropertyString", "AttachedPart", "StraightCut", "Object which links to Part")
+        obj.addProperty("App::PropertyString", "AttachedTool", "StraightCut", "Object which links to Tool")
         obj.Proxy = self
 
     def execute(self, obj):
         doc = FreeCAD.ActiveDocument
         sel = FreeCADGui.Selection.getSelection()
+        # s[part|tool] = selected; l[part|tool] = linked to
+        part = None
+        spart = None
+        tool = None
+        stool = None
+        linked = False
+        # Initial generation
         if obj.Part is None and obj.Tool is None:
             if len(sel) == 2:
-                obj.Part = sel[0]
-                obj.Tool = sel[1]
+                spart = sel[0]
+                stool = sel[1]
+                if spart.TypeId == 'App::Link' and stool.TypeId == 'App::Link':
+                    obj.AttachedPart = spart.Name
+                    part = spart.LinkedObject
+                    obj.AttachedTool = stool.Name
+                    tool = stool.LinkedObject
+                    linked = True
+                else:
+                    part = spart
+                    tool = stool
             else:
-                FreeCAD.Console.PrintError("Must select two bodies")
+                FreeCAD.Console.PrintError("Must select two bodies or links to bodies")
                 return
-        com = obj.Part.Shape.common(obj.Tool.Shape)
+        # Recomputation
+        else:
+            # Updating to tip of the body if we're not pointed at it anymore.
+            tpart = obj.Part.getParent().Tip
+            if tpart != obj.Part:
+                if tpart != obj:
+                    obj.Part = tpart
+                else:
+                    obj.Part = obj.BaseFeature
+            obj.Tool = obj.Tool.getParent().Tip
+            # If we have a link that points to the part (assume the same for tool)
+            if obj.AttachedPart != "":
+                spart = doc.getObject(obj.AttachedPart)
+                stool = doc.getObject(obj.AttachedTool)
+                linked = True
+
+        # If the part and tool are a body (probably the case on inital creation)
+        if part.TypeId == 'PartDesign::Body' and tool.TypeId == 'PartDesign::Body':
+            # Make doubly sure the tip isn't us
+            if part.Tip == obj:
+                part = obj.BaseFeature
+            else:
+                part = part.Tip
+            tool = tool.Tip
+
+        if not part.isDerivedFrom('PartDesign::Feature') or not tool.isDerivedFrom('PartDesign::Feature'):
+            FreeCAD.Console.PrintError("can't use selection")
+            return
+
+
+        # common shape, which will be generated differently in different scenarios
+        com = None
+        # the easy mode
+        if not linked:
+            com = part.Shape.common(tool.Shape)
+        # the hard mode
+        else:
+            fixed = None
+            attached = None
+            lattached = None
+            # we have to figure out which was attached to which
+            # sometimes the part can be attached to the tool
+            if spart.AttachedTo.split('#')[0] == stool.Name:
+                fixed = tool
+                lattached = spart
+                attached = part
+            elif stool.AttachedTo.split('#')[0] == spart.Name:
+                fixed = part
+                lattached = stool
+                attached = tool
+            else:
+                FreeCAD.Console.PrintError("not attached")
+                return
+            placement = lattached.LinkPlacement
+            pat = doc.getObject(lattached.AttachedTo.split('#')[0])
+            while pat != None:
+                v1 = pat.Placement.Base
+                v2 = placement.Base
+                r1 = pat.Placement.Rotation
+                r2 = placement.Rotation
+                placement.Base = v2 - v1
+                placement.Rotation = r2.multiply(r1.inverted())
+                pat = doc.getObject(pat.AttachedTo.split('#')[0])
+            temp = FreeCAD.ActiveDocument.addObject("Part::Feature", "TempBody")
+            temp.Shape = attached.Shape
+            temp.Placement = placement
+            com = fixed.Shape.common(temp.Shape)
+            doc.removeObject(temp.Name)
         comp = FreeCAD.ActiveDocument.addObject("Part::Feature", "Common")
         comp.Shape = com
         cl = comp.Shape.BoundBox.ZLength
@@ -31,10 +117,12 @@ class StraightCut():
         doc.removeObject(comp.Name)
         face = Part.Face(wire)
         extr = face.extrude(FreeCAD.Vector(0, 0, cl))
-        cut = obj.Part.Shape.cut(extr)
+        cut = part.Shape.cut(extr)
         obj.Shape = cut
-        obj.Part.Parents[0][0].addObject(obj)
-        obj.Part.Visibility = False
+        obj.Part = part
+        obj.Tool = tool
+        if obj.getParent() == None:
+            part.getParent().addObject(obj)
 
 class ViewProviderStraightCut:
     def __init__(self, obj):
@@ -44,12 +132,6 @@ class ViewProviderStraightCut:
     def attach(self, obj):
         self.Object = obj.Object
         return
-
-    def claimChildren(self):
-        objs = []
-        objs.append(self.Object.Part)
-        objs.append(self.Object.Tool)
-        return objs
 
     def updateData(self, fp, prop):
         return
@@ -86,7 +168,7 @@ class AddStraightCut:
 
   def Activated(self):
     doc = FreeCAD.ActiveDocument
-    obj = doc.addObject('Part::FeaturePython', "StraightCut")
+    obj = doc.addObject('PartDesign::FeaturePython', "StraightCut")
     sel = FreeCADGui.Selection.getSelection()
     StraightCut(obj)
     ViewProviderStraightCut(obj.ViewObject)
